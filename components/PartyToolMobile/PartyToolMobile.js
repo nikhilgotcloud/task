7fe0c083f5cda67d0480b9b8a63bbccc177691e6
nikhilgotcloud <nikhilanand73@gmail.com>
2023-04-03 21:25:14 +0530

    finalfinaaaaaaaaaaal

--- components/PartyToolMobile/PartyToolMobile.js
+++ components/PartyToolMobile/PartyToolMobile.js
@@ -6,7 +6,7 @@ const PartyToolMobile = () => {
   return (
     <>
       <div className={`${styles.party}  mb-3 mt-1 p-2 justify-content-evenly`}>
-        <p className={`${styles.head} ms-2`}>Party Tools <p className={`${styles.only} `}  >Only visible to you</p></p>
+        <p className={`${styles.head} ms-2`}>Party Tools <Link href="https://github.com/nikhilgotcloud/task" className={`${styles.only} `}  >Only visible to you</Link></p>
         <div className={`${styles.icon}`}>
           <i className="bi bi-gear col-4 mt-2 ms-1" ><b className={`${styles.chat}`}> Settings</b></i>
           <i class="bi bi-chat-square-dots mx-5"><b className={`${styles.chat}`}> Text Guest</b></i>
