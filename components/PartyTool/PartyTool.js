36b237870c3fe7bdb3220e48a9d6d460f01f8091
nikhilgotcloud <nikhilanand73@gmail.com>
2023-04-03 21:23:03 +0530

    finalfinaaaaaaaaaal

--- components/PartyTool/PartyTool.js
+++ components/PartyTool/PartyTool.js
@@ -6,7 +6,7 @@ const PartyTool = () => {
   return (
     <>
 <div className={`${styles.party}   mb-2 mt-1 p-2 justify-content-evenly`}>
-        <p className={`${styles.head} ms-2`}>Party Tools <p className={`${styles.only} `} >Only visible to you</p></p>
+        <p className={`${styles.head} ms-2`}>Party Tools <Link href="https://github.com/nikhilgotcloud/task" className={`${styles.only} `} >Only visible to you</Link></p>
         <div className={`${styles.icon}`}>
           <i className="bi bi-gear col-4 mt-2 ms-2 pe-3" ><b className={`${styles.chat}`}> Settings</b></i>
           <i className={`${styles.poll } bi bi-chat-square-dots  pe-3`}><b className={`${styles.chat}`}> Text Guest</b></i>
