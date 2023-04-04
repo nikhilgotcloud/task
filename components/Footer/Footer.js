7d26c4b599d4efb12db7e7f4f78ea518b612a726
nikhilgotcloud <nikhilanand73@gmail.com>
2023-04-04 09:42:32 +0530

    finalfinaaaaaaaaaaaaaaal

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,16 +5,16 @@ import Image from "next/image";
 
 const Footer = () => {
   useEffect(() => {
-    ReactDOM.hydrate(
+    ReactDOM.render(
       <div className="fixed-bottom ">
         <div className="container">
           <div className="row">
             <div className={`${styles.footer} col p-2`}>
               <div className={`${styles.icon} justify-content-evenly`}>
-                <Image className=" mx-1 p-2" src="/homeft.png " width={40} height={40} alt="qw"></Image>
-                <Image className=" mx-3 p-2 " src="/chat.png " width={45} height={40} alt="qw"></Image>
-                <Image className=" mx-2 p-2" src="/save.png " width={35} height={35} alt="qw"></Image>
-                <Image className=" mx-3  p-2" src="/arrow-up-from-square.png " width={35} height={35} alt="qw"></Image>
+                <Image className=" mx-1 p-2" src="/homeft.png" width={40} height={40} alt="qw" />
+                <Image className=" mx-3 p-2 " src="/chat.png" width={45} height={40} alt="qw" />
+                <Image className=" mx-2 p-2" src="/save.png" width={35} height={35} alt="qw" />
+                <Image className=" mx-3  p-2" src="/arrow-up-from-square.png" width={35} height={35} alt="qw" />
                 <button className={`${styles.button} p-1  border-0 text-light  p-2 ps-3 pe-3 `}>Shop</button>
               </div>
             </div>
